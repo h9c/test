064c654483dfcc57b200ab8e39e18b229284ee74
sergejcervakov <h9c@ya.ru>
2019-02-06 15:34:02 +0600

    PHP v.1.19.0

--- php/19_anonymous_functions.php
+++ php/19_anonymous_functions.php
@@ -5,27 +5,53 @@
  * Date: 08/01/2019
  * Time: 15:25
  */
-    $hello = function ($string) {
-        echo "$string test<br>";
-    };
-    $hello ('world, затем идет слово - ');
 
-    $hellotwo = function ($string) {
-        $a = 2;
-        $b = 2;
-        $c = $a * $b;
-        echo "$string 2+2=<b>$c</b><br>";
-    };
-    $hellotwo ('World; ');
-    $hellotwo ('');
-    $hello ('');
-
-    function test($a, $b) {
-        $arr = [];
-        for ($i = 0; $i < $a; $i++) {
-            $arr[] = $b($i);
+function dash($a = 0)
+{
+    if ($a == 1) :
+        echo '<br>';
+    elseif ($a == 0) :
+        echo '<br>';
+        for ($i = 0; $i < 5; $i++) {
+            echo ' --- --- --- ';
         }
-        return $arr;
+        echo '<br>';
+    else: echo 'иначе';
+    endif;
+}
+
+dash();
+
+$a = function ($a = 'Если не задан параметр, по умолчанию выводим эту строку') {
+    echo $a . ' Текст. ';
+};
+$a ('Выводим переменную $a = ');
+dash();
+
+echo $a('Выводим переменную $a ') . $a();
+dash();
+
+$b = function ($a = '$b + $c = $d') {
+    $b = 4;
+    $c = 5;
+    $d = $b + $c;
+    echo $a . $b . ' + ' . $c . ' = ' . $d;
+};
+
+$b('Равно ');
+dash();
+
+function test($a, $b)
+{
+    $arr = [];
+    for ($i = 0; $i < $a; $i++)
+    {
+        $arr[] = $b($i);
     }
-    $arr = test(10, 2);
-    echo $arr;
+    return $arr;
+}
+$arr = test(6 , function($x) {
+    return $x * $x;
+
+});
+foreach ($arr as $v) echo $v.', ';
